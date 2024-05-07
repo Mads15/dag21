7c246dd45874bff75d62a56fb08149441f2d6cda
Mads15 <madskodeloftet@gmail.com>
2024-05-07 10:38:06 +0200

    1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+// [] er array
+// {} er object
+
+const myAPI = [
+    {frukt: "Epple", godt: "ja"},
+    {frukt: "Banan", godt: "nei"},
+    {frukt: "Appelsin", godt: "ja"}
+];
+
+
+console.log(myAPI[1].frukt);
+console.log(myAPI[1].godt);
